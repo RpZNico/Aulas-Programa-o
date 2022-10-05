268e11d90a6a421e569e7396c89987c5a4cc547e
Nicolas Matheus <nicolasmsb@gmail.com>
2022-10-04 22:25:37 -0300

    termino da aula 10 e começo do exerciocio 11

---
+++ Javascript-Aulas/Aula 10 Operadores aritméticos, de atribuicao e incremento/index.js
@@ -0,0 +1,36 @@
+/* 
+Aritméticos + - / *
+*  + Adição / Concatenação
+*  ** - Potencia
+*  % Resto da divisão
+*  Precedencia () > ** > * > / > % > + > -
+* Incremento = ++    Pode vir antes ou depois do contador, se vir antes ele soma e executa o comando, se vir depois ele primeiro executa o comando depois ele apresenta
+* Decremento = --
+ */
+
+/* let contador = 1;
+console.log(++contador) 
+console.log(contador) */
+
+
+/* let contador =1;
+contador++;
+console.log(contador++) */
+
+
+/* // Operadores de atribuição
+let contador = 2;
+contador **= 10 // contador = contador + passo
+
+console.log(contador)
+ */
+
+
+// NaN - Not a Number
+// parseInt = converte me número inteiro
+// parseFlow = converte em número com casas decimais
+// number = ja faz tudo isso
+const num1 = 10;
+const num2 = Number('5.2');
+console.log(num1 + num2);
+console.log(typeof num2)
