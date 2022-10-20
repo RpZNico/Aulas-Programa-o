024901ec136405d7dfc944723fb1400a12fe6486
Nicolas Matheus <nicolasmsb@gmail.com>
2022-10-19 22:12:57 -0300

    fazendo mais um pouco da atividade

--- Javascript-Aulas/Aula 26 Exercício unindo tudo aprendido/modelo/assets/js/main.js
+++ Javascript-Aulas/Aula 26 Exercício unindo tudo aprendido/modelo/assets/js/main.js
@@ -4,35 +4,37 @@ function meuEscopo () {
     
 
     function recebeEventoForm (evento) {
-        const peso = form.querySelector('.declarapeso');
-        const altura = form.querySelector('.declaraaltura')
+        const peso = form.querySelector('#declarapeso').value;
+        const altura = form.querySelector('#declaraaltura').value;
+
 
         evento.preventDefault();
-        const numero = (peso / altura * altura);
+        const numero = (peso / (altura * altura));
 
     if (numero <= 18.5) {
-    console.log('Você está abaixo do peso');
+     ('Você está abaixo do peso');
 } else if (numero >= 18.6 && numero <= 24.9) {
-    console.log('Você está com o peso normal');
+    Text = 'Você está com o peso normal';
 } else if (numero >= 25 && numero <= 29.9) {
-    console.log('Você está com sobrepeso'); 
+    Text = 'Você está com sobrepeso'; 
 } else if (numero >= 30 && numero <= 34.9) {
-    console.log('Obesidade grau 1');
+    Text = 'Obesidade grau 1';
 } else if (numero >= 35 && numero <= 39.9) {
-    console.log('Obesidade grau 2');
+    Text = 'Obesidade grau 2';
 } else if (numero >= 40) {
-    console.log('Obesidade grau 3');
-} else if (numero !== isNaN ) {
-    console.log('Coloque apenas numeros');
-} else if (numero <= 0) {
-    console.log('Ocorreu um erro');
-}
-
-resultado.innerHTML += `${numero}`
+    Text = 'Obesidade grau 3';
+} else if (numero === isNaN ) {
+    Text = 'Coloque apenas numeros';
+} else {
+    Text = 'Ocorreu um erro';
 }
 
 
- form.addEventListener('submit', recebeEventoForm);
+}
+
+
+form.addEventListener('submit', recebeEventoForm);
+
 }
 
 meuEscopo();
