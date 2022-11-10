61ad179edb91241b66cc0e7a28bb2cb0b29c384a
Nicolas Matheus <nicolasmsb@gmail.com>
2022-11-09 22:44:07 -0300

    tentando fazer a atividade

--- Javascript-Aulas/Aula 34 Exercicio For/assets/js/main.js
+++ Javascript-Aulas/Aula 34 Exercicio For/assets/js/main.js
@@ -1,11 +1,22 @@
-const elementos = [
+/* const section = document.querySelector('.container h1') */
+
+let elementos = [
     {tag: 'p', texto: 'Frase 1'}, // 0
     {tag: 'div', texto: 'Frase 2'}, // 1 
     {tag: 'footer', texto: 'Frase 3'}, // 2
     {tag: 'section', texto: 'Frase 4'}, // 3
 ]
 
-const [1] = 
-for (let i = 0; i < elementos.length; i ++) {
+
+/* for (let i = 0; i < elementos.length; i ++) {
    console.log(`Indice ${i}`, elementos[3]);
+} */
+
+/* 
+for (var {tag,  texto } of elementos) {
+    console.log(tag, texto);
 }
+
+
+
+section.innerHTML += tag, texto */
